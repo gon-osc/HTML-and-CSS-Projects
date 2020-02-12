ca8a634daf9f15b08e7b89dde47e2bd4929b287c
Oscar Gonzalez <gonzalezoscarsalvador@gmail.com>
2020-02-11 18:27:18 -0700

    HTML and Javascript Assignments

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World!_I_am_an_alert_box!");
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}	
